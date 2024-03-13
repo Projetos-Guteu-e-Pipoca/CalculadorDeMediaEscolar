ca48d111ec90f9078265b0c642164931506b85c7
Mendes <114362709+ThatOneMendes@users.noreply.github.com>
2024-03-13 14:25:02 -0300

    Add files via upload

--- medias-escolares/sartre/satre1em/satre1em-1.js
+++ medias-escolares/sartre/satre1em/satre1em-1.js
@@ -25,11 +25,18 @@ const calculosBimestre = {
 
 const calculosEspeciais = {
     "red": {
-        "1": "notas.ad + notas.ao",
-        "2": "notas.ad + notas.ao",
-        "3": "notas.ad + notas.ao",
-        "4": "notas.ad + notas.ao"
+        "1": "( notas.ad + notas.ao ) / 2",
+        "2": "( notas.ad + notas.ao ) / 2",
+        "3": "( notas.ad + notas.ao ) / 2",
+        "4": "( notas.ad + notas.ao ) / 2"
     }
+}
+
+const pesosUnidades = {
+    "1": 2,
+    "2": 3,
+    "3": 2,
+    "4": 3
 }
 
 const botaoCalcular = document.getElementById("calcularNota");
@@ -87,7 +94,7 @@ function mostarNota(mat) {
     // Alterar a nota final.
 
     notaDisplay.innerText = nota.toFixed(2);
-    ppDisplay.innerText = nota.toFixed(2) * 10;
+    ppDisplay.innerText = nota.toFixed(2) * (pesosUnidades[unidade] ?? 2); // Caso o peso da unidade não possa ser encontrado, usar 2 como o peso padrão!
 
     // Decidir as cores.
 
@@ -99,21 +106,27 @@ function mostarNota(mat) {
 // porém inputs + um input de type "submit" já cuida de valores ilegais para nós, então
 // essa função foi de vasco!
 
-const setas_contato = document.getElementsByClassName("seta-contato")
-const texts_contato = document.getElementsByClassName("texto-contato")
-const nomes_contato = document.getElementsByClassName("nome-contato")
-let c = []
-for (let i=0;i<setas_contato.length; i++){
-    c[i] = 0
-    setas_contato[i].innerHTML = "<span class='fa-solid fa-arrow-down'></span>"
-    texts_contato[i].style.display = "none"
-    nomes_contato[i].addEventListener("click", function (){
-        c[i]++
-        if ((c[i]/2)%1!=0){
-            texts_contato[i].style.display = "block"
-            setas_contato[i].innerHTML = "<span class='fa-solid fa-arrow-up'></span>"
-        }else{
-            texts_contato[i].style.display = "none"
-            setas_contato[i].innerHTML = "<span class='fa-solid fa-arrow-down'></span>"
-    }
-})}
+// Pegar todas as setas.
+const setasContato = document.getElementsByClassName("fa-arrow-down");
+
+// Dar loop pelas setas
+for(seta of setasContato) {
+    // Pegar o id da seta, dividir em um array e pegar o ultimo elemento ([].pop() tira o ultimo elemento do array e retorna o elemento removido,
+    // nesse caso, pode ser "pipoca" ou "guteu")
+    const displayName = seta.id.split("-").pop();
+
+    // Return caso o array esteja vazio! (o que faz o pop retornar undefined)
+    if(displayName === undefined) continue;
+
+    // Pegar a caixa de contato.
+    const displayElement = document.getElementById(`texto-contato-${displayName}`)
+
+    // Adicionar o event click.
+    seta.addEventListener("click", () => {
+        // Muda a imagem da seta e a visibilidade da caixa de contato.
+        displayElement.style.display = displayElement.style.display === "none" ? "block" : "none";
+        // Temos que usar document.getElementById já que mudanças no objeto seta não refletem no HTML.
+        const setaHTML = document.getElementById(`seta-contato-${displayName}`);
+        setaHTML.className = setaHTML.className === "fa-solid fa-arrow-down" ? "fa-solid fa-arrow-up" : "fa-solid fa-arrow-down";
+    });
+}
